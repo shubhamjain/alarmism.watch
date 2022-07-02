0577f5add2a1a83e98dd2ed7daa85c539cf249b5
Shubham Jain <shubham.jain.1@gmail.com>
2022-07-02 18:53:16 +0530

    New commit

--- fetch.sh
+++ fetch.sh
@@ -3,6 +3,7 @@
 CONTENTS="$(curl -sL "https://docs.google.com/spreadsheets/d/15hRWjG6zTUY1kwTBMMLnBPqtIZ64eEY-sxoCYRQQObg/gviz/tq?tqx=out:json&sheet=Sheet1")"
 
 CLEAN_JSON="$(echo "$CONTENTS" | sed 's/\/\*.*\*\///g' | sed -E 's|google.visualization.Query.setResponse\((.*)\);|\1|')"
+LAST_MODIFIED="$(date '+%B %d, %Y')"
 FORMATTED_JSON="$(echo "$CLEAN_JSON" | jq '.table.rows[] | {
     headline: .c[0].v,
     link: .c[1].v,
@@ -11,10 +12,6 @@ FORMATTED_JSON="$(echo "$CLEAN_JSON" | jq '.table.rows[] | {
     category: .c[4].v,
     about_year: .c[5].v,
     year: .c[6].v
-}' | jq -s '{items: .}')"
+}' | jq -s 'sort_by(.about_year)' | jq "{items: ., last_modified: \"$LAST_MODIFIED\"}")"
 
 jinja2 root.html <(echo "$FORMATTED_JSON") --format=json > index.html
-
-git add .
-git commit -m "New commit"
-git push origin
